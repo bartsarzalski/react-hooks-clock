06511cc5b40672cb3a0cda39163d225388d79937
bartek <bartek.sarzalski@gmail.com>
2021-03-21 15:40:33 +0100

    first and last update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+import './App.css'
+
+const App = () => {
+  const deg = 6;
+  let day = new Date();
+  const [hour, setHour] = useState(0);
+  const [minute, setMinute] = useState(0);
+  const [second, setSecond] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setHour(day.getHours() * 30);
+      setMinute(day.getMinutes() * deg);
+      setSecond(day.getSeconds() * deg);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [hour, minute, second]);
+
+  const hourStyle = {
+    transform: `rotateZ(${(hour) + (minute/12)}deg)`
+  }
+
+  const minuteStyle = {
+    transform: `rotateZ(${(minute)}deg)`
+  }
+
+  const secondStyle = {
+    transform: `rotateZ(${(second)}deg)`
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="clock">
+      <div className="hour" style={hourStyle} />
+      <div className="minute" style={minuteStyle} />
+      <div className="second" style={secondStyle} />
+    </div>);
 }
+
 
 export default App;
